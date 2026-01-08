1d3dac67c1a7d4a0509d25085ba06814587ba521
ikiwblack <44847107+ikiwblack@users.noreply.github.com>
2026-01-09 01:35:04 +0700

    Refactor apply_emotion to include blinking and nodding

--- scripts/svg_emotion.py
+++ scripts/svg_emotion.py
@@ -1,24 +1,22 @@
 from lxml import etree
+from scripts.svg_facial import apply_blink, apply_head_nod
 
-def apply_emotion(svg_path, output_path, emotion):
+NS = {"svg": "http://www.w3.org/2000/svg"}
+
+def apply_emotion(svg_path, output_path, emotion, t=0.0):
     tree = etree.parse(svg_path)
     root = tree.getroot()
 
-    ns = {"svg": "http://www.w3.org/2000/svg"}
-
-    mouth = root.xpath("//*[@id='mouth']", namespaces=ns)[0]
-    head = root.xpath("//*[@id='head']", namespaces=ns)[0]
+    mouth = root.xpath("//*[@id='mouth']", namespaces=NS)[0]
 
     if emotion == "happy":
         mouth.attrib["d"] = "M236 200 Q256 220 276 200"
-
     elif emotion == "sad":
         mouth.attrib["d"] = "M236 210 Q256 190 276 210"
-
     elif emotion == "angry":
         mouth.attrib["d"] = "M236 205 L276 205"
 
-    elif emotion == "thinking":
-        head.attrib["transform"] = "rotate(-10 256 180)"
+    apply_blink(root, t)
+    apply_head_nod(root, t, emotion)
 
     tree.write(output_path)
