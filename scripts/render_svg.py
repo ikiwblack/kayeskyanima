e5450da9addcf8c83249639ebbe8e7a199a3b79e
ikiwblack <44847107+ikiwblack@users.noreply.github.com>
2026-01-09 02:05:48 +0700

    Delete scripts/render_svg.py

--- scripts/render_svg.py
+++
@@ -1,9 +0,0 @@
-import cairosvg
-
-def svg_to_png(svg_string, out_path, w, h):
-    cairosvg.svg2png(
-        bytestring=svg_string.encode(),
-        write_to=out_path,
-        output_width=w,
-        output_height=h
-    )
